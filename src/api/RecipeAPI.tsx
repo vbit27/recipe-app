ee959d4ecbf60040b7c821eaf37e665832281c95
Vasil Bit <vasil.bituni@gmail.com>
2021-11-11 13:04:49 +0100

    Add meal and diet to API call

--- src/api/RecipeAPI.tsx
+++ src/api/RecipeAPI.tsx
@@ -5,7 +5,13 @@ export const getRecipe = async (
   meal?: string[],
   diet?: string[]
 ) => {
-  const url = createURL(ingredient, meal, diet);
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=${
+    process.env.REACT_APP_API_ID
+  }&app_key=${process.env.REACT_APP_API_KEY}${addDietType(diet)}${addMealType(
+    meal
+  )}
+  &random=true&field=uri&field=label&field=image&field=source&field=url&field=healthLabels
+  &field=ingredientLines&field=calories&field=totalTime&field=cuisineType&field=mealType`;
 
   try {
     const data = await axios.get(url);
@@ -17,22 +23,14 @@ export const getRecipe = async (
   }
 };
 
-const createURL = (ingredient: string, meal: any, diet: any) => {
-  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=${
-    process.env.REACT_APP_API_ID
-  }&app_key=${process.env.REACT_APP_API_KEY}${addDietType(diet)}${addMealType(
-    meal
-  )}
-  &random=true&field=uri&field=label&field=image&field=source&field=url&field=healthLabels
-  &field=ingredientLines&field=calories&field=totalTime&field=cuisineType&field=mealType`;
-
-  return url;
+const addMealType = (meals: string[] | undefined) => {
+  if (meals) {
+    return meals.map((meal: string) => `&mealType=${meal}`).join('');
+  }
 };
 
-const addMealType = (meals: any) => {
-  return meals.map((meal: any) => `&mealType=${meal}`).join('');
-};
-
-const addDietType = (diet: any) => {
-  return diet.map((diet: any) => `&health=${diet}`).join('');
+const addDietType = (diet: string[] | undefined) => {
+  if (diet) {
+    return diet.map((diet: string) => `&health=${diet}`).join('');
+  }
 };
